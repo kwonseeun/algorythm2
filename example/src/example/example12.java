1826b68989dfead2702b7d53fca7883d0d03aa1b
kwonseeun <0115km@naver.com>
2022-03-11 15:27:51 +0900

    substr 예제

---
+++ example/src/example/example12.java
@@ -0,0 +1,20 @@
+package example;
+
+public class example12 {
+
+	public static void main(String[] args) {
+		
+	String strTest = "01234567890123456789";
+
+	String strResult = strTest.substring(0,5);
+	
+	System.out.println(strResult);
+	
+	String strResult2 = strTest.substring(8);
+
+	System.out.println(strResult2);
+	
+	
+	}
+
+}
