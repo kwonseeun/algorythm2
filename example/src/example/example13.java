5e9edcd27c3c82aacd5b6838b01bcc4b1b328276
kwonseeun <0115km@naver.com>
2022-03-12 23:26:52 +0900

    split 예제 추가

---
+++ example/src/example/example13.java
@@ -0,0 +1,15 @@
+package example;
+
+public class example13 {
+
+	public static void main(String[] args) {
+		String str2 = "서울,대전,대구,부산,인천,울산";
+		String[] cityArr = str2.split(",");
+		
+		for (int i = 0; i < cityArr.length; i++) {
+			System.out.println(cityArr[i]);
+		}
+
+	}
+
+}
