7ae348343a024f61ede0d08ce539e542889689a9
kwonseeun <0115km@naver.com>
2022-03-21 09:25:42 +0900

    example08 수정 0-09

--- example/src/algorythm3/example08.java
+++ example/src/algorythm3/example08.java
@@ -1,12 +1,14 @@
 package algorythm3;
 
-/** 0부터 10까지 더한값을 출력*/
+/** 0부터 10까지 더한값을 출력하고 홀수인 값만 더하기*/
 public class example08 {
 
 	public static void main(String[] args) {
 		int sum = 0;
 		for (int i = 0; i <= 10 ; i++) {
-			sum += i ;
+			if (i%2==1) {
+				sum += i;
+			}
 		}
 		System.out.println("더한 총 값 " + sum );
 	}
