09fbd638d90c37293d4451988e3cee6f6dc02515
kwonseeun <0115km@naver.com>
2022-03-21 09:23:57 +0900

    example08 추가 1~10 더한 합

---
+++ example/src/algorythm3/example08.java
@@ -0,0 +1,14 @@
+package algorythm3;
+
+/** 0부터 10까지 더한값을 출력*/
+public class example08 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 0; i <= 10 ; i++) {
+			sum += i ;
+		}
+		System.out.println("더한 총 값 " + sum );
+	}
+
+}
