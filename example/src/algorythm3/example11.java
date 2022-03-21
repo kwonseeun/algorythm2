652a20b5752016e7ce17f6511fd803a044012734
kwonseeun <0115km@naver.com>
2022-03-21 09:44:26 +0900

    example11 추가

---
+++ example/src/algorythm3/example11.java
@@ -0,0 +1,26 @@
+package algorythm3;
+
+import java.util.Scanner;
+
+/**피라미드 역순으로 출력하기
+ *****
+ ****
+ ***
+ **
+ **/
+public class example11 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = sc.nextInt();
+		sc.close();
+		
+		for (int i = 0; i < sum; i++) {
+			for (int j = sum; j > i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
