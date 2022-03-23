eefb800485f607cc0d682cd67a0538667072c2ab
kwonseeun <0115km@naver.com>
2022-03-23 17:55:29 +0900

    SubString 예제 추가

---
+++ example/src/algorythm4/SubStringEx01.java
@@ -0,0 +1,12 @@
+package algorythm4;
+
+public class SubStringEx01 {
+
+	public static void main(String[] args) {
+		String str = "Hello";
+		
+		System.out.println(str.substring(2, 4)); 
+		System.out.println(str.substring(5));
+	}
+
+}
