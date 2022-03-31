6ac90031006fdf48f8108c2e73b2063202b3ae13
kwonseeun <0115km@naver.com>
2022-03-31 15:01:36 +0900

    replaceAll 예제

---
+++ example/src/algorythm4/exampleReplaceAll.java
@@ -0,0 +1,13 @@
+package algorythm4;
+
+public class exampleReplaceAll {
+
+	public static void main(String[] args) {
+		String a = "abcdefg";
+		
+		a = a.replaceAll("a", "d");
+		
+		System.out.println(a);
+	}
+
+}
