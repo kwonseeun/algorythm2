4750500ce22db986f47772b50dd435fa2bb732ef
kwonseeun <0115km@naver.com>
2022-03-31 14:54:14 +0900

    문자열 자르기

---
+++ example/src/algorythm4/example.java
@@ -0,0 +1,13 @@
+package algorythm4;
+
+public class example {
+	public static void main(String[] args) {
+		String str= "ABCDEF";
+		
+		System.out.println(str.substring(3));
+		
+		System.out.println(str.substring(3, 6));
+		
+		System.out.println(str.substring(3, str.length()));
+				}
+}
