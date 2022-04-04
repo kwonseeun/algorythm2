220b2e9813ba84b285f61fd98834544823e5d585
kwonseeun <0115km@naver.com>
2022-04-04 15:10:32 +0900

    array test

---
+++ example/src/algorythm4/ArraysExample.java
@@ -0,0 +1,12 @@
+package algorythm4;
+
+public class ArraysExample {
+
+	public static void main(String[] args) {
+
+	String[] arr = {"abc", "def" , "ghi"};
+	
+	System.out.println(arr.toString());
+	}
+
+}
