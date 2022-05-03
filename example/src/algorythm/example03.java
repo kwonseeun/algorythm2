f8c3018aa5d8ce548fd234618c172493a5b23987
kwonseeun <0115km@naver.com>
2022-05-03 17:42:45 +0900

    계단코드

--- example/src/algorythm/example03.java
+++ example/src/algorythm/example03.java
@@ -1,15 +1,24 @@
 package algorythm;
 
+import java.util.Scanner;
+
 public class example03 {
 
 	public static void main(String[] args) {
-	
-		for (int h = 1; h <= 23 ; h++) {
-			for (int m = 1; m < 59; m++) {
-				for (int s = 1; s < 59; s++) {
-					System.out.printf(h+"시"+m+ "분"+ s+"초");
-				}
+		
+		// 입력 받은 숫자로 숫자 계단을 출력하기 
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt(); 
+		
+		int cnt =1;
+		
+		for (int i = 0; i <= num; i++) { // i=1부터 입력받은 숫자 num 까지 
+			for (int j = 0; j <i ; j++) { // j=i 부터 i까지 (i가 1이면 한번 2이면 두번)  
+				
+				System.out.print(String.format("%3d", cnt));
+				cnt++;
 			}
+			System.out.println();
 		}
 	}
 
