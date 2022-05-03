c286c1292943ab677a5f1d8c5a5549863109ff08
kwonseeun <0115km@naver.com>
2022-05-03 17:59:16 +0900

    for문 추가

--- example/src/algorythm/example01.java
+++ example/src/algorythm/example01.java
@@ -2,18 +2,16 @@ package algorythm;
 
 public class example01 {
 
-/** 1 ~ 100 까지 숫자 중 3과 5의 배수를 출력하고 그 합을 구하시오. */
-public static void main(String[] args) {
-		
-		int sum = 0;
-		
-		// 1 ~ 100까지 숫자 중 3과 5의 배수 출력
-		for(int i = 1; i <= 100; i++) {
-			if(i % 3 == 0 && i % 5 == 0) {
-				sum += i;
-			System.out.print(i + " ");
-			}
-		}	
-		System.out.println("\n합계: " + sum);
+	/** 배열을 이용한 로또 출력 */
+	public static void main(String[] args) {
+	 
+		int[] score;
+		score = new int[10];
+		for (int i = 0; i < score.length; i++) {
+			score[i] = i+1;
+		}
+		for (int i = 0; i < score.length; i++) {
+			System.out.println(score[i] = i+1);
+		}
 	}
 }
