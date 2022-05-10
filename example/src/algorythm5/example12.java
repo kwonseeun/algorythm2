dbc5aa95d369d017cb2983ee7a9bf76393629012
kwonseeun <0115km@naver.com>
2022-05-10 13:40:28 +0900

    글자 거꾸로 출력

--- example/src/algorythm5/example12.java
+++ example/src/algorythm5/example12.java
@@ -1,19 +1,27 @@
 package algorythm5;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Scanner;
 
 public class example12 {
 
 	public static void main(String[] args) {
-		String word = "ABCDEF";
-		String[] array_world; // 스트링을 담을 배열 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("단어를 입력해주세요:");
+		String word =sc.next();
 		
-		array_world= word.split(""); //배열에 한글자씩 저장하
+		String[] array_word = new String[word.length()]; //스트링을 담을 배열 
+		String result = ""; // 출력할 스트링변수 
 		
-		for (int i = 0; i < array_world.length; i++) {
-			System.out.println(array_world[i]);
+		for (int i = 0; i < array_word.length; i++) { //string 한글자씩 끊어 배열에 저장 
+			array_word[i]=Character.toString(word.charAt(i));
 		}
+		
+		for (int i = array_word.length-1; i>=0; i--) {
+			result+=array_word[i];
+		}
+		
+		System.out.println(result);
+		
 	}
 
 }
