a0aae21ef703f107df0c130ac6134b66e5f1510b
kwonseeun <0115km@naver.com>
2022-05-14 12:28:35 +0900

    선택 알고리즘

--- example/src/selectSort/example03.java
+++ example/src/selectSort/example03.java
@@ -1,22 +1,26 @@
 package selectSort;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class example03 {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = 1000 - Integer.parseInt(br.readLine());
-		int[] coinTypes = {500,100,50,10,5,1};
-		int count = 0;
-		
-		for (int coin : coinTypes) {
-			count += n/ coin;
-			n %= coin;
+	public static void main(String[] args) {
+		int[] arr = {30,10,40,20,50};
+		System.out.print("정렬 전 \n");
+		for (int i : arr) {
+			System.out.println(i);
 		}
-		System.out.println(count);
+		System.out.println("\n 정렬 후 ");
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+				} 
+			}
+		}
+		for (int i : arr) {
+			System.out.println(" " +i);
+		}
 	}
 
 }
---
+++ example/src/selectSort/example04.java
@@ -0,0 +1,14 @@
+package selectSort;
+
+import java.util.Scanner;
+
+public class example04 {	
+	public static void main(String[] args) {
+		int arr[] = new int[10];
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0; i < 10; i++) {
+			arr[i] = sc.nextInt();
+		}
+	}
+}
