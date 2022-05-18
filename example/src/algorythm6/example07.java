a22dc6cc81a857e33add22b73ab4adbf694edab7
kwonseeun <0115km@naver.com>
2022-05-18 14:38:01 +0900

    replace,substring,split

--- example/src/algorythm6/example07.java
+++ example/src/algorythm6/example07.java
@@ -7,9 +7,23 @@ public class example07 {
 		String a = "aaaabbbb";
 		String b = "aa-aa,bb-bb";
 		
+		/** replace 예제*/
 		System.out.println("문자열" + a);
 		System.out.println("a를 c로 바꾸기" + a.replace("a", "c"));
 		System.out.println("ab를 c로 바꾸기" + a.replace("ab", "c"));
+		
+		/**subString*/
+		System.out.println("subString(4)" + a.substring(4));
+		System.out.println("subString(0,4)" + a.substring(0, 4));
+		System.out.println("\n문자열 :" +b+ "\n---,로 문자열 분리---");
+		String[] str = b.split(",");
+		for (String temp : str) 
+			System.out.println(temp);
+		
+		System.out.println("\n문자열 : " + b + "\n---와 -로 문자열 분리 ");
+		String[] str2 = b.split(",|-");
+		for (String temp : str2) 
+			System.out.println(temp);
 	}
 
 }
