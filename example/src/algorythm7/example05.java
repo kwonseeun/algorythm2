f66e70359fdd8175b54d99cd741f23193219866f
kwonseeun <0115km@naver.com>
2022-06-04 23:28:42 +0900

    n진수 게임

--- example/src/algorythm7/example05.java
+++ example/src/algorythm7/example05.java
@@ -1,10 +1,24 @@
 package algorythm7;
 
-public class example05 {
+import java.util.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder sb = new StringBuilder();
+        
+        p--; //0번부터 시작하기 위해
+        
+        String temp = ""; // n진수 변환 스트링
+        for(int i = 0; temp.length() < t*m; i++){ // t번 고를 숫자까지 변환
+            temp += Integer.toString(i,n).toUpperCase();
+        }
+        
+        // t개 고르기
+        for(int i = 0; i < t; i++){
+            sb.append(temp.charAt(i*m+p));
+        }
+        
+        return sb.toString();
+    }
+    
 }
