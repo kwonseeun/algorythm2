10638219ab306894cbfc750bb87c720997e95711
kwonseeun <0115km@naver.com>
2022-06-06 00:00:14 +0900

    add

--- example/src/algorythm7/example06.java
+++ example/src/algorythm7/example06.java
@@ -1,6 +1,17 @@
 package algorythm7;
 
 class example06 {
+	/**
+    1. n*n배열 생성(최종 정답 배열은 n*(n+1)/2)
+    
+    2. 높이 증가하면서 0이거나 배열안이면 +1씩 증가된 숫자 넣기
+    
+    3. 그다음 가로방향 증가(0이거나 배열안이면)
+    
+    4. 가로 -1 높이 -1 하면서 움직이기
+    
+    5. 2~4번 반복하며 진행
+**/
 	
     public int[] solution(int n) {
         int[][] map = new int[n][n];
@@ -22,6 +33,17 @@ class example06 {
                 map[x][y] = num++;
             }
         }
-        return null;
+        
+        
+        int[] answer = new int[n*(n+1)/2];
+        int idx = 0;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                answer[idx++] = map[i][j];
+            }
+        }
+        
+        return answer;
     }
 }
