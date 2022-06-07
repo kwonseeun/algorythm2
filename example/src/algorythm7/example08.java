7850d5ee50d4cd4ae388a263ef89e4bae8d59243
kwonseeun <0115km@naver.com>
2022-06-07 15:26:18 +0900

    피보나

--- example/src/algorythm7/example08.java
+++ example/src/algorythm7/example08.java
@@ -2,9 +2,14 @@ package algorythm7;
 
 public class example08 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public int solution(int n) {
+		int a =0; int b =1;int c=0;
+		
+		for(int i =2; i<=n; i++) {
+			c = (a+b)%1234567;
+			a = b%1234567;
+			b = c%1234567;
+		}
+		return c;
+  }
 }
