002c37c7f8f163f3f6692e326ece02e5134d4580
kwonseeun <0115km@naver.com>
2022-06-17 13:46:24 +0900

    정수 내림차순

---
+++ example/src/example/example14.java
@@ -0,0 +1,18 @@
+package example;
+
+public class example14 {
+
+		public long solution(long n) {
+			long answer = 0;
+				String strResult ="";
+				
+			
+			return ;
+		}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
