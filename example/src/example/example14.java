995ee5db2e87e6856d57b160666ff74c7d24d3bf
kwonseeun <0115km@naver.com>
2022-06-17 13:48:55 +0900

    add

--- example/src/example/example14.java
+++ example/src/example/example14.java
@@ -1,13 +1,28 @@
 package example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class example14 {
 
 		public long solution(long n) {
 			long answer = 0;
 				String strResult ="";
-				
+				String strn = String.valueOf(n);
+				List<String> slist = Arrays.asList(strn.split(""));
+				List<Integer> ilist = new ArrayList<Integer>();
+				for(String s : slist) {
+					ilist.add(Integer.parseInt(s));
+				}
+				ilist.sort(new Comparator<Integer>() {
+					@Override
+					public int compare(Integer o1, Integer o2) {
+						return 02-01;
+					}
+				});
 			
-			return ;
 		}
 	
 	public static void main(String[] args) {
