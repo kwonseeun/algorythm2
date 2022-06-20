cfeedeaa1d3e2ac2d20bab541765eebe9eec105c
kwonseeun <0115km@naver.com>
2022-06-20 22:39:17 +0900

    bubble sorting

--- example/src/bubbleSort/example09.java
+++ example/src/bubbleSort/example09.java
@@ -3,8 +3,17 @@ package bubbleSort;
 public class example09 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] a = {254,3,213,64,75,56,4,324,65,78,9,5,76,3410,8,342,76};
+		int b;
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length-i-1; j++) {
+				if(a[j]>a[j+1]) {
+					b = a[j];
+					a[j] = a[j+1];
+					a[j+1] = b;
+				}
+			}
+		}
 	}
 
 }
