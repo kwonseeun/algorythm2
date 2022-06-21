53073537a23c4c0a18bb3f69dcdacb141728908f
kwonseeun <0115km@naver.com>
2022-06-21 22:30:28 +0900

    thread 상속

--- example/src/java01/ThreadTest.java
+++ example/src/java01/ThreadTest.java
@@ -1,6 +1,17 @@
 package java01;
 
-
+class MyThread extends Thread{
+	
+	public MyThread(String threadName){
+		super(threadName);
+	}
+	public void run(){
+		for(int i=0;i<100;i++){
+			System.out.println(this.getName()+":"+i);
+		}
+		System.out.println();
+	}
+}
 public class ThreadTest {
 
 	public static void main(String[] ar){
