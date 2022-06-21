2681d508482420eeb677552e1e1d627c98e6fc1e
kwonseeun <0115km@naver.com>
2022-06-21 22:30:14 +0900

    add

--- example/src/java01/ThreadTest.java
+++ example/src/java01/ThreadTest.java
@@ -1,5 +1,14 @@
 package java01;
 
+
 public class ThreadTest {
 
+	public static void main(String[] ar){
+		System.out.println("MainThread Start");
+		for(int i=1;i<=3;i++){
+			new MyThread("Thread"+i).start();
+		}
+		System.out.println("MainThread End");	
+	}
+	
 }
