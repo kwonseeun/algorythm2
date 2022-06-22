41505c815bd75b8867a87fde7518ae9259183d42
kwonseeun <0115km@naver.com>
2022-06-22 22:56:15 +0900

    switch문 추가 각 월의 일 구하기

--- example/src/miniProject/Cal.java
+++ example/src/miniProject/Cal.java
@@ -17,8 +17,25 @@ public class Cal {
 		
 		if(leapYear(year)) {
 			switch(year) {
-			
+			 case 1: case 3: case 5: case 7: case 8: case 10: case 12: 
+		          tmp = 31; 
+		        break; 
+		        case 4: case 6: case 9: case 11: 
+		          tmp = 30; 
+		        break; 
+		        case 2: 
+		          tmp = 29; 
+		        break; 
+			}
+		} else {
+			if(month == 1 || month ==3 || month == 5 || month == 7 ||month == 8 || month == 10 || month == 12) {
+				tmp = 31;
+			} else if(month == 4 || month == 6 || month ==9 || month == 11) {
+				tmp = 39;
+			} else if (month == 2) {
+				tmp = 28;
 			}
 		}
+		return tmp;
 	}
 }
