57a3116e81fb9a9dfbb68295dee7f564364d1f4e
kwonseeun <0115km@naver.com>
2022-06-22 22:54:15 +0900

    add

--- example/src/miniProject/Cal.java
+++ example/src/miniProject/Cal.java
@@ -3,12 +3,22 @@ package miniProject;
 public class Cal {
 	
 	// 윤년 계산
-	private static boolean leapyear(int year) {
+	private static boolean leapYear(int year) {
 		boolean leap = false;
 		
 		if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) { 
 		      leap = true; 
 		}
 		return true;
+	}
+	
+	private static int getDate(int year, int month) {
+		int tmp = 0;
+		
+		if(leapYear(year)) {
+			switch(year) {
+			
+			}
+		}
 	}
 }
