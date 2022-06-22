98f663f546e6b9436d79c4dfaaa8cf8cd7e028e1
kwonseeun <0115km@naver.com>
2022-06-22 22:53:20 +0900

    윤년 계산 메소드 추가

--- example/src/miniProject/Cal.java
+++ example/src/miniProject/Cal.java
@@ -1,5 +1,14 @@
 package miniProject;
 
 public class Cal {
-
+	
+	// 윤년 계산
+	private static boolean leapyear(int year) {
+		boolean leap = false;
+		
+		if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) { 
+		      leap = true; 
+		}
+		return true;
+	}
 }
