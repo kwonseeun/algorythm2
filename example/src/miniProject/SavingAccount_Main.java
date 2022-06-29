f7422a32be94acd41aecb535cd96305a65c73179
kwonseeun <0115km@naver.com>
2022-06-29 23:12:38 +0900

    연이율

--- example/src/miniProject/SavingAccount_Main.java
+++ example/src/miniProject/SavingAccount_Main.java
@@ -3,8 +3,23 @@ package miniProject;
 public class SavingAccount_Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SavingAccount balance1 = new SavingAccount(10000);
+		SavingAccount balance2 = new SavingAccount (20000);
+		
+		System.out.println("첫 달 연이율: 0.05");
+		System.out.println("첫 번째 계좌 이자:" + (int) balance1.getInterest()+"");
+		System.out.println("두 번째 계좌 이자:" + (int) balance1.getInterest()+"");
+		
+		balance1.setInterest_rate(0.06);
+		balance2.setInterest_rate(0.06);
 
+		System.out.println("두 번째 달 연이율: 0.06");
+		System.out.println("\n★두 번째 달 - 연이율 : 0.06");
+		  System.out.println("첫 번째 계좌 이자 : "+(int)balance1.getInterest()+"원");
+		  System.out.println("두 번째 계좌 이자 : "+(int)balance2.getInterest()+"원");
+		  
+		  System.out.println("\n★첫 번째 계좌 총 잔액 : "+balance1.getCurrent_balance()+"원");
+		  System.out.println("★두 번째 계좌 총 잔액 : "+balance2.getCurrent_balance()+"원");
 	}
-
 }
