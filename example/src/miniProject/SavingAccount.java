95fb78173bf1fcf0ef95192949494caf48a74bb4
kwonseeun <0115km@naver.com>
2022-06-29 23:05:36 +0900

    getInterest

--- example/src/miniProject/SavingAccount.java
+++ example/src/miniProject/SavingAccount.java
@@ -6,6 +6,13 @@ public class SavingAccount {
 	double interest;
 	
 	public SavingAccount(int curent_balance) {
-		
+		this.interest_rate = 0.05;
+		this.interest = current_balance*interest_rate/12;
+		this.current_balance = current_balance+(int)interest;
+	}
+	
+	public double getInterest() {
+		this.interest = current_balance*interest_rate/12;
+		return interest;
 	}
 }
