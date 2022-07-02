dc43e2faa368ff8b175ef340c76d9ce3790c7044
kwonseeun <0115km@naver.com>
2022-07-03 02:10:27 +0900

    공배수 구하

--- example/src/algorythm7/example14.java
+++ example/src/algorythm7/example14.java
@@ -18,6 +18,15 @@ public class example14 {
 	static int lcm( int a, int b ) {
 		return a*b / gcd(a, b);
 	}
-	
+	static int gcd( int a, int b ) {
+		
+		while (b!=0) {
+			int c = a%b;
+			a=b;
+			b=c;
+		}
+		
+		return a;
+	}
 
 }
