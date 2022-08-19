20bafa603dc62e55e1c26131673785a2435aa639
kwonseeun <0115km@naver.com>
2022-08-20 00:27:32 +0900

    배열 for 문

---
+++ example/src/algorythm7/example19.java
@@ -0,0 +1,18 @@
+package algorythm7;
+
+import java.util.Scanner;
+
+public class example19 {
+	public static void main(String[] args) {
+		System.out.println("숫자를 입력하세요: ");
+		
+		Scanner sc = new Scanner(System.in);
+		int cnt = sc.nextInt();
+		int[] data = new int [cnt];
+		
+		for (int i = 0; i < data.length; i++) {
+			data[i] = (i + 1);
+		}
+	}
+
+}
