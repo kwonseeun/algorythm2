bb6f985c5288a679816f0181dfea402444d950ef
kwonseeun <0115km@naver.com>
2022-08-21 23:39:14 +0900

    update

--- example/src/algorythm7/example20.java
+++ example/src/algorythm7/example20.java
@@ -10,6 +10,23 @@ public class example20 {
 	public static void main(String[] args) {
 		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	        
+	        String[] lr = br.readLine().split(" ");
+
+	        char[] l = lr[0].toCharArray();
+	        char[] r = lr[1].toCharArray();
+	        
+	        int answer = 0;
+	        if(l.length == r.length) {
+	            for (int i = 0; i < l.length; i++) {
+	                if(l[i] == r[i] && l[i] == '8') {
+	                    answer++;
+	                }else if(l[i] != r[i]){
+	                    break;
+	                }
+	            }
+	        }
+
 	}
 
 }
