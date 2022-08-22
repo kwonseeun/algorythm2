a51e066a0602da03adb48518084a2fd57467fa5b
kwonseeun <0115km@naver.com>
2022-08-22 23:12:42 +0900

    문자열 출력

---
+++ example/src/algorythm7/example21.java
@@ -0,0 +1,23 @@
+package algorythm7;
+
+public class example21 {
+	public boolean solution(String s) {
+		boolean answer = true;
+		
+		if (s.length()!=4 && s.length() != 6) {
+			return false;
+			
+		}else {
+			try {
+				int temp = Integer.parseInt(s);
+			} catch (Exception e) {
+				return false;
+			}
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
