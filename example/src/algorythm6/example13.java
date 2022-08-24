f05bd37a6baa6420e02c52dce8faa7fb6e8ab841
kwonseeun <0115km@naver.com>
2022-08-24 23:36:49 +0900

    update

--- example/src/algorythm6/example13.java
+++ example/src/algorythm6/example13.java
@@ -2,16 +2,29 @@ package algorythm6;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class example13 {
 
 	public static void main(String[] args) {
 		
 		// HashMap 선언 
-		Map<String,Object> map = new HashMap<String,Object>();
-		map.put("1", 5);
-		map.put("2", 70);
-		map.put("3", 50);
+		Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+		map.put(1, 5);
+		map.put(2, 70);
+		map.put(3, 50);
+		
+		// Max
+		Entry<Integer,Integer> maxEntry = null;
+		
+		// Iterator
+		Set<Entry<Integer,Integer>> entrySet = map.entrySet();
+		for (Entry<Integer, Integer> entry : entrySet) {
+			if (maxEntry == null || entry.getValue() > maxEntry.getValue()) {
+				maxEntry = entry;
+			}
+		}
 	}
 
 }
