b92fa8ee64cfef21011cccfffe8658fc518db9b9
kwonseeun <0115km@naver.com>
2022-08-24 23:39:36 +0900

    key기준 최소/최대값 찾기

---
+++ example/src/algorythm6/example14.java
@@ -0,0 +1,25 @@
+package algorythm6;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class example14 {
+
+	// key 기준 최대 / 최소값 찾기 
+	public static void main(String[] args) {
+		
+	Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+	
+	map.put(1, 5);
+	map.put(2, 70);
+	map.put(3, 30);
+	
+	// Max Key
+	Integer maxKey = Collections.max(map.keySet());
+	
+	// Min Key
+	Integer minKey = Collections.min(map.keySet());
+	}
+
+}
