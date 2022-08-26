a709119e11d8ac115397f8aae37011f2af6cc5c6
kwonseeun <0115km@naver.com>
2022-08-26 23:55:54 +0900

    update

--- example/src/algorythm6/example16.java
+++ example/src/algorythm6/example16.java
@@ -1,9 +1,15 @@
 package algorythm6;
 
+import java.util.StringTokenizer;
+
 public class example16 {
 
-	public static void main(String[] args) {
-		
+	public static void StringTokenizerTest (String str) {
+		StringTokenizer st = new StringTokenizer(str, " ");
+		for (int i = 0; st.hasMoreTokens(); i++) {
+			System.out.println(i + "번쨰: " + st.nextToken());
+			
+		}
 	}
 
 }
