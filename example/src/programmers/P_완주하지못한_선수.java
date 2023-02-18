918def5caf898cce39304ab9ad3d243808a6104d
kwonseeun <0115km@naver.com>
2023-02-18 23:52:25 +0900

    update

--- example/src/programmers/P_완주하지못한_선수.java
+++ example/src/programmers/P_완주하지못한_선수.java
@@ -1,5 +1,26 @@
 package programmers;
 
 public class P_완주하지못한_선수 {
-
+	 public String solution(String[] participant, String[] completion) {
+	        String answer = "";
+	        boolean[] check = new boolean[participant.length];
+	        
+	        for(int i = 0 ; i < completion.length ; i++){
+	           Loop1 : for(int j = 0 ; j < participant.length ;j++){
+	                if(!check[j]&&completion[i].equals(participant[j])){
+	                    check[j]=true;
+	                    break Loop1;
+	                }
+	            }
+	        }
+	        
+	        for(int i = 0 ; i < participant.length ; i ++){
+	            if(!check[i])
+	                answer = participant[i];
+	        }
+	        
+	        
+	        
+	        return answer;
+	    }
 }
