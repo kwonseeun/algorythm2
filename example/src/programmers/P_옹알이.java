8744c41b8522d74b353c745c547eaff61679206a
kwonseeun <0115km@naver.com>
2023-02-19 22:54:43 +0900

    프로그래머스_옹알

---
+++ example/src/programmers/P_옹알이.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class P_옹알이 {
+	 public int solution(String[] babbling) {
+	        int answer = 0;
+	        String[] data = {"aya","ye","woo","ma"};
+	        
+	        for(int i=0; i<babbling.length; i++){
+	            for(int j=0; j<data.length; j++){
+	                babbling[i] = babbling[i].replace(data[j]," ");
+	            }
+	            
+	            babbling[i] = babbling[i].trim();
+	            if(babbling[i].length() == 0){
+	                answer +=1;
+	            }
+	        }
+	        return answer;
+	    }
+}
