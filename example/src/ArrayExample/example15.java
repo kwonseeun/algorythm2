7b824f1fe99fcbd5af3014d232188c68d925b20c
kwonseeun <0115km@naver.com>
2023-03-18 22:46:58 +0900

    프로그래머스_전화번호 목록

---
+++ example/src/ArrayExample/example15.java
@@ -0,0 +1,21 @@
+package ArrayExample;
+
+public class example15 {
+	class Solution {
+	    public boolean solution(String[] phone_book) {
+	        boolean answer = true;
+	        
+	        for(int i=0; i<phone_book.length; i++){
+	            for(int j=0; j<phone_book.length; j++){
+	                if(i==j) continue;
+	                if(phone_book[j].startsWith(phone_book[i])){
+	                    answer = false;
+	                    break;
+	                }
+	            }
+	            if(!answer)
+	                break;
+	        }
+	        return answer;
+	    }
+}
