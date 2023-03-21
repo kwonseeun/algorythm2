3d09aab6c15546dfb35aebf3c0710f09440d8b31
kwonseeun <0115km@naver.com>
2023-03-21 22:31:22 +0900

    프로그래머스_괄호

--- example/src/ArrayExample/example16.java
+++ example/src/ArrayExample/example16.java
@@ -1,5 +1,44 @@
 package ArrayExample;
 
+import java.util.Scanner;
+
 public class example16 {
 
+	public static String isValidPS(String ps) {
+		
+		String result = "";
+		int len = ps.length();
+		int cnt = 0;
+		
+		for(int i=0; i<len; i++)
+		{
+			if(ps.charAt(i) == '(')
+				cnt++;
+			else
+				cnt--;
+			
+			if(cnt<0)
+			{
+				result = "NO";
+				break;
+			}
+		}
+		
+		if(cnt==0)
+			result = "YES";
+		else
+			result = "NO";
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-- > 0) {
+			System.out.println(isValidPS(sc.next()));
+		}
+	}
 }
