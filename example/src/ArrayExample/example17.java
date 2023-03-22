ff3760782de0769212906b459ac3d1e81473b42e
kwonseeun <0115km@naver.com>
2023-03-22 23:19:50 +0900

    백준 수들의합

---
+++ example/src/ArrayExample/example17.java
@@ -0,0 +1,30 @@
+package ArrayExample;
+
+import java.util.Scanner;
+
+public class example17 {
+
+	public static void main(String[] args) {
+		
+		 Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+	        int answer = 0;
+
+	        int start = 1;
+	        int end = 1;
+	        int sum = 1;
+
+	        while(start <= end){
+	            if(sum == n) answer++;
+	            if(sum < n){
+	                end++;
+	                sum+=end;
+	            }
+	            else if(sum >= n){
+	                sum -= start;
+	                start++;
+	            }
+	        }
+	        System.out.println(answer);
+		}
+	}
