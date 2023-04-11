01d87b86a3aa92f009eef9874466f4ca613bab4d
kwonseeun <0115km@naver.com>
2023-04-11 22:53:04 +0900

    프로그래머스_기능개발

--- example/src/ArrayExample/example28.java
+++ example/src/ArrayExample/example28.java
@@ -28,7 +28,7 @@ public class example28 {
 
 	        for (int i = 0; i < answer.length; i++) {
 	            answer[i] = answerList.get(i);
-	        }
+	        } 
 
 	        return answer;
 	    }
