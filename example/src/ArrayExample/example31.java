c5700022b4804ce9d9f4bfcca8d93885ab9c0f8f
kwonseeun <0115km@naver.com>
2023-04-16 22:44:14 +0900

    프로그래머스-카드뭉치

---
+++ example/src/ArrayExample/example31.java
@@ -0,0 +1,20 @@
+package ArrayExample;
+
+public class example31 {
+	 public String solution(String[] cards1, String[] cards2, String[] goal) {
+	        int i = 0;
+	        int j = 0;
+	        int k = 0;
+	        while(k<goal.length){
+	            if(i < cards1.length&&goal[k].equals(cards1[i])){
+	                k++;
+	                i++;
+	            } else if(j < cards2.length&&goal[k].equals(cards2[j])){
+	                k++;
+	                j++;
+	            } else break;
+	        }
+	        
+	        return k==goal.length ? "Yes" : "No";
+	    }
+}
